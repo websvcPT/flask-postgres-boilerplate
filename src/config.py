845e6b7244aa5c2809042e899c1be93344ccb2d0
Nelson Dias <nelson@websvc.net>
2018-04-15 19:27:57 +0100

    add files

---
+++ src/config.py
@@ -0,0 +1,24 @@
+import os, logging
+
+DEBUG = True
+HOST = os.getenv('HOST', '0.0.0.0')
+PORT = int(os.getenv('PORT', '5000'))
+
+POSTGRES = {
+    'user': os.getenv('POSTGRES_USER', 'postgres'),
+    'pw': os.getenv('POSTGRES_PW', ''),
+    'host': os.getenv('POSTGRES_HOST', os.getenv('DB_PORT_5432_TCP_ADDR')),
+    'port': os.getenv('POSTGRES_PORT', os.getenv('DB_PORT_5432_TCP_PORT')),
+    'db': os.getenv('POSTGRES_DB', 'postgres'),
+}
+DB_URI = 'postgresql://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+
+logging.basicConfig(
+    filename=os.getenv('SERVICE_LOG', 'server.log'),
+    level=logging.DEBUG,
+    format='%(levelname)s: %(asctime)s pid:%(process)s module:%(module)s %(message)s',
+    datefmt='%d/%m/%y %H:%M:%S',
+)
+
+SUPERHERO_API_URL = os.getenv('HOST', '127.0.0.1:5001')
+
